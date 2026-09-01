0e950f00a040382ee2a064f8982cca46124ce08b
Abhisek Bhuyan <141822548+abhisekbhuyan@users.noreply.github.com>
2026-09-02 04:03:09 +0530

    Delete SpringBootApplication directory

--- SpringBootApplication/src/middleware/api/auth.js
+++
@@ -1,9 +0,0 @@
-
-//1. login
-export const login=(id)=>{
-    return `http://localhost:9090/integra/api/employee/validate/${id}`
-}
-
-export const submitCateenData = () => {
-    return `http://localhost:9090/integra/api/employee/create`
-}
